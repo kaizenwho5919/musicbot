1f4f4b0a1af698f9d975c9a8623b7f00fe8d759e
kaizenwho5919 <pathakarjun438@gmail.com>
2025-03-23 12:52:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
         clientId: "a568b55af1d940aca52ea8fe02f0d93b",
         clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
     },
-    botToken: "MTM1Mjk0NzA1ODY5NjUyMzg3Nw.GcOxtS.Op-b5Zdgur1smPgv_3voWuXPjETR4JLflQMKXw",
+    //botToken: "",
     embedColor: "#0061ff"
 };
