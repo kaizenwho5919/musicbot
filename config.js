4e5e407b666cf3fc52e4c3bd11d7ae2e3c0ccce9
kaizenwho5919 <pathakarjun438@gmail.com>
2025-03-23 12:51:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    prefix: '!',
+    nodes: [{
+        host: "lavalink.jirayu.net",
+        password: "youshallnotpass",
+        port: 13592,
+        secure: false,
+        name: "Main Node"
+    }],
+    spotify: {
+        clientId: "a568b55af1d940aca52ea8fe02f0d93b",
+        clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
+    },
+    botToken: "MTM1Mjk0NzA1ODY5NjUyMzg3Nw.GcOxtS.Op-b5Zdgur1smPgv_3voWuXPjETR4JLflQMKXw",
+    embedColor: "#0061ff"
+};
